8745c72366f6c62d2865184ea28764809bd0c20e
David Pocknee <dappy_iodo@hotmail.com>
2018-08-10 19:54:43 +0100

    all fucked up

--- src/Block.js
+++ src/Block.js
@@ -8,6 +8,18 @@ const blockSource = {
     return {id: props.id}
   }
 };
+
+const difficultycolors = [
+  'rgb(255, 0, 0)', 
+  'rgb(191, 64, 0)',
+  'rgb(223, 32, 0)',
+  'rgb(159, 96, 0)',
+  'rgb(128, 128, 0)',
+  'rgb(96, 159, 0)',
+  'rgb(64, 191, 0)',
+  'rgb(32, 223, 0)',
+  'rgb(0, 255, 0)'
+];
    
 function collect(connect, monitor) {
   return {
@@ -18,17 +30,37 @@ function collect(connect, monitor) {
 
 class Block extends Component {
   render() {
-    var eachImage = ('/chords/'+ this.props.value + '.png');
+    var eachImage = ('/chords/'+ this.props.name + '.png');
+    var speedcolor = ('rgb('+ this.props.redvalue + ', ' + this.props.greenvalue + ', 0)');
     const { connectDragSource, isDragging } = this.props;
     return connectDragSource(
       <div style={{
         opacity: isDragging ? 0.5 : 1,
-        fontSize: 10,
         textAlign: 'center',
         cursor: 'move'
       }}>
+          <div style={{
+            height: '20px',
+            width: '100%',
+            backgroundColor: speedcolor,
+            fontSize: 10,
+            color: 'white',
+            fontWeight: 'bold',
+            textAlign: 'center',
+          }} >
+          <p>{this.props.name}</p>
+        </div>
+        <div style={{
+          height: '30px',
+          width: '100%',
+          backgroundColor: difficultycolors[this.props.difficulty-1],
+          fontSize: 8,
+          color: 'white',
+          textAlign: 'center',
+        }} >
+        <p>Difficulty: {this.props.difficulty}/9</p>
+        </div>
         <img src={eachImage} alt={'right'}/>
-        {this.props.value}
       </div>
     );
   }
--- src/Grid.js
+++ src/Grid.js
@@ -4,6 +4,7 @@ import HTML5Backend from 'react-dnd-html5-backend';
 import GridSquare from './GridSquare';
 import Block from './Block';
 import jsondata from './outputdb.json';
+import timingdata from './timing.json';
 
 const squareWidth = '60px';
 const squareHeight = '250px';
@@ -12,15 +13,19 @@ var blockFrom = null;
 
 export function sourcerer(value,id) {
   blockFrom = [value,id]
+  console.log("sourcerer " + value + ' ' + ' ' + id)
 }
 
 var mainarray = []
+var timing = []
 
 console.log(jsondata['chords'][0])
 for (let i = 0; i < jsondata['chords'].length; i++) {
-  var outter = jsondata['chords'][i].name;
-  mainarray.push(outter);
+  mainarray.push([jsondata['chords'][i].name,i]);
+  timing.push(timingdata['timing'][i]);
 };
+
+//console.log(timing)
 
 var starting = null;
 
@@ -29,27 +34,47 @@ class Grid extends Component {
 
       if (starting != null) {
         var blockId = blockFrom[1]
-        var oldPosition = mainarray.findIndex(function(x) {return x===blockId;});
+        console.log(" blockFrom[0] " + blockFrom[0] + " blockFrom[1] " + blockFrom[1])
+        var outty = null;
+        console.log(mainarray)
+        var oldPosition = mainarray.findIndex(function(x,y) {          
+          if (x === blockId){
+              outty = [x,y]
+              }
+          console.log("outty " + outty)
+          return outty;
+           });
+        console.log("Old Position " + oldPosition[0] + ' ' + oldPosition[1])
         var newPosition = this.props.blockPosition[0]
-        mainarray.splice(oldPosition,1)
-        mainarray.splice(newPosition,0,blockId)
+        console.log("New Position " + newPosition)
+        mainarray.splice(oldPosition[1],1)
+        mainarray.splice(newPosition,0,[blockId,oldPosition[0]],)
+
       } else {
+        console.log(mainarray)
         starting = true
       }
 
     var current_order_string = [];
     const squares = [];
 
-    for (let index = 0; index < mainarray.length; index++) {
+    for (let index = 0; index < 5; index++) {
+      var currentvalue = mainarray[index][0]
+      var currentindex = mainarray[index][1]
       squares.push(
           <div key={index}
                style={{ width: squareWidth, height: squareHeight, margin: '10px 5px 20px 5px',border: '0px solid black'}}>
-            <GridSquare index={index} value={mainarray[index]} swidth={squareWidth} sheight={squareHeight}>
-              <Block id={mainarray[index]} value={mainarray[index]}/>
+            <GridSquare index={index} value={currentvalue} swidth={squareWidth} sheight={squareHeight}>
+              <Block id={currentvalue} 
+              name={currentvalue} 
+              redvalue={timing[index][3]}
+              greenvalue={timing[index][4]}
+              />
             </GridSquare>
           </div>
         );
-      current_order_string.push(mainarray[index] + ' ');
+      current_order_string.push(currentvalue + ' ');
+      console.log(currentindex)
     }
     return (
       <div>
@@ -68,8 +93,8 @@ class Grid extends Component {
 }
 
 //   <div><p>Order: {current_order_string}</p></div>
-
-
-
+//difficulty ={jsondata['chords'][i]} 
+//notes ={jsondata['chords'][i]} 
+//harmonic_ratio ={jsondata['chords'][i]
 
 export default DragDropContext(HTML5Backend)(Grid);
