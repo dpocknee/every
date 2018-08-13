5be2fed932be127fdb10d0aab9a0c8816ce60078
David Pocknee <dappy_iodo@hotmail.com>
2018-08-13 20:13:02 +0100

    working webaudio.  Next is to re-record the samples and add them in.

---
+++ src/webaudio.js
@@ -0,0 +1,287 @@
+timing = {"timing":[                   
+[ 1.155 , 3.25  , 1.000 , 255 , 0 ],
+[ 2.405 , 2.901 , 0.978 , 249 , 6 ],
+[ 3.306  , 1.355 , 0.942 , 240 , 15  ],
+[ 5.661  , 0.639 , 0.894 , 228 , 27  ],
+[ 6.067  , 2.819 , 0.756 , 193 , 62  ],
+[ 7.886  , 1.904 , 0.676 , 172 , 83  ],
+[ 8.79 , 1.107 , 0.598 , 152 , 103 ],
+[ 9.897  , 2.473 , 0.528 , 135 , 120 ],
+[ 10.37 , 1.981 , 0.466 , 119 , 136 ],
+[ 12.351  , 1.618 , 0.414 , 106 , 149 ],
+[ 13.969  , 1.337 , 0.369 , 94  , 161 ],
+[ 15.306  , 1.128 , 0.332 , 85  , 170 ],
+[ 16.434  , 0.966 , 0.300 , 77  , 178 ],
+[ 17.4  , 0.835 , 0.271 , 69  , 186 ],
+[ 18.235  , 0.732 , 0.247 , 63  , 192 ],
+[ 18.967  , 0.652 , 0.225 , 57  , 198 ],
+[ 19.619  , 0.584 , 0.206 , 52  , 203 ],
+[ 20.203  , 0.529 , 0.188 , 48  , 207 ],
+[ 20.732  , 0.483 , 0.172 , 44  , 211 ],
+[ 21.215  , 0.445 , 0.158 , 40  , 215 ],
+[ 21.66 , 0.413 , 0.145 , 37  , 218 ]
+]};
+
+
+jsondata = {"chords":[
+  {
+    "name" : "chord_001", 
+    "difficulty" : 8,
+    "notes" : 4,
+    "harmonics" : 0,
+    "harmonic_ratio" : 0,
+    "octaves" : [1, 3, 3, 4, 0],
+    "octavehistogram" : [1, 0, 2, 1, 0],
+    "notation" : ["e,\\6", "e'\\5", "e'\\4", "e''\\1"],
+    "files" : ["string-6_e1_octave-1", "string-5_e3_octave-3", "string-4_e3_octave-3", "string-1_e4_octave-4"],
+    "buffer_reference" : [0, 5, 9, 14]
+  },
+  {
+    "name" : "chord_002", 
+    "difficulty" : 8,
+    "notes" : 3,
+    "harmonics" : 0,
+    "harmonic_ratio" : 0,
+    "octaves" : [3, 3, 4, 0, 0],
+    "octavehistogram" : [0, 0, 2, 1, 0],
+    "notation" : ["e'\\5", "e'\\4", "e''\\1"],
+    "files" : ["string-5_e3_octave-3", "string-4_e3_octave-3", "string-1_e4_octave-4"],
+    "buffer_reference" : [5, 9, 14]
+  },
+  {
+    "name" : "chord_003", 
+    "difficulty" : 9,
+    "notes" : 3,
+    "harmonics" : 0,
+    "harmonic_ratio" : 0,
+    "octaves" : [2, 3, 4, 0, 0],
+    "octavehistogram" : [0, 1, 1, 1, 0],
+    "notation" : ["e\\6", "e'\\5", "e''\\2"],
+    "files" : ["string-6_e2_octave-2", "string-5_e3_octave-3", "string-2_e4_octave-4"],
+    "buffer_reference" : [1, 5, 12]
+  },
+  {
+    "name" : "chord_004", 
+    "difficulty" : 7,
+    "notes" : 3,
+    "harmonics" : 0,
+    "harmonic_ratio" : 0,
+    "octaves" : [3, 4, 3, 0, 0],
+    "octavehistogram" : [0, 0, 2, 1, 0],
+    "notation" : ["e'\\5", "e''\\2", "e'\\1"],
+    "files" : ["string-5_e3_octave-3", "string-2_e4_octave-4", "string-1_e3_octave-3"],
+    "buffer_reference" : [5, 12, 13]
+  },
+  {
+    "name" : "chord_005", 
+    "difficulty" : 7,
+    "notes" : 3,
+    "harmonics" : 0,
+    "harmonic_ratio" : 0,
+    "octaves" : [2, 4, 4, 0, 0],
+    "octavehistogram" : [0, 1, 0, 2, 0],
+    "notation" : ["e\\6", "e''\\2", "e''\\1"],
+    "files" : ["string-6_e2_octave-2", "string-2_e4_octave-4", "string-1_e4_octave-4"],
+    "buffer_reference" : [1, 12, 14]
+  },
+  {
+    "name" : "chord_006", 
+    "difficulty" : 7,
+    "notes" : 3,
+    "harmonics" : 0,
+    "harmonic_ratio" : 0,
+    "octaves" : [2, 3, 4, 0, 0],
+    "octavehistogram" : [0, 1, 1, 1, 0],
+    "notation" : ["e\\6", "e'\\4", "e''\\1"],
+    "files" : ["string-6_e2_octave-2", "string-4_e3_octave-3", "string-1_e4_octave-4"],
+    "buffer_reference" : [1, 9, 14]
+  },
+  {
+    "name" : "chord_007", 
+    "difficulty" : 7,
+    "notes" : 3,
+    "harmonics" : 0,
+    "harmonic_ratio" : 0,
+    "octaves" : [3, 3, 4, 0, 0],
+    "octavehistogram" : [0, 0, 2, 1, 0],
+    "notation" : ["e'\\4", "e'\\3", "e''\\1"],
+    "files" : ["string-4_e3_octave-3", "string-3_e3_octave-3", "string-1_e4_octave-4"],
+    "buffer_reference" : [9, 10, 14]
+  },
+  {
+    "name" : "chord_008", 
+    "difficulty" : 7,
+    "notes" : 3,
+    "harmonics" : 0,
+    "harmonic_ratio" : 0,
+    "octaves" : [2, 2, 4, 0, 0],
+    "octavehistogram" : [0, 2, 0, 1, 0],
+    "notation" : ["e\\6", "e\\5", "e''\\1"],
+    "files" : ["string-6_e2_octave-2", "string-5_e2_octave-2", "string-1_e4_octave-4"],
+    "buffer_reference" : [1, 4, 14]
+  }  
+  ]};
+
+class Sounder {  
+  constructor(context, buffer) {
+    this.context = context;
+    this.buffer = buffer;
+  }
+  
+  setup() {
+    this.gainNode = this.context.createGain();
+    this.source = this.context.createBufferSource();
+    this.source.buffer = this.buffer;
+    this.source.connect(this.gainNode);
+    this.gainNode.connect(this.context.destination);
+    
+    this.gainNode.gain.setValueAtTime(0.8, this.context.currentTime);
+  }
+
+  play(time) {
+    this.setup();
+    this.source.start(time);
+   }
+  
+  stop() {
+    var ct = this.context.currentTime + 0.5;
+    this.gainNode.gain.exponentialRampToValueAtTime(0.001, ct);
+    this.source.stop(ct);
+  }
+}
+
+
+//THIS CLASS IS FOR LOADING ALL SAMPLES INTO SEPARATE BUFFERS
+class Buffer {
+  constructor(context, urls) {  
+    this.context = context;
+    this.urls = urls;
+    this.buffer = [];
+  }
+  
+  loadSound(url, index) {
+    let request = new XMLHttpRequest();
+    request.open('get', url, true);
+    request.responseType = 'arraybuffer';
+    let thisBuffer = this;
+    request.onload = function() {
+      // Safari doesn't support promise based syntax
+      thisBuffer.context
+        .decodeAudioData(request.response, function(buffer) {
+          thisBuffer.buffer[index] = buffer;
+          if(index == thisBuffer.urls.length-1) {
+            thisBuffer.loaded();
+          }       
+        });
+    };
+    request.send();
+  };
+  
+  getBuffer() {
+    this.urls.forEach((url, index) => {
+      this.loadSound(url, index);
+    })
+  }
+   
+  getSound(index) {
+    return this.buffer[index];
+  }
+
+}
+
+//THIS DEFINES THE AUDIO CONTEXT AND AN ARRAY OF ALL SOUNDS TO BE LOADED INTO THE BUFFERS
+let context = new (window.AudioContext || window.webkitAudioContext)();
+let sines = [
+	'sine/01.wav',
+	'sine/02.wav',
+	'sine/03.wav',
+	'sine/04.wav',
+	'sine/05.wav',
+	];
+
+let sounds = [
+      'samples/019-1.wav',
+      'samples/019-2.wav',
+      'samples/019-3.wav',
+      'samples/019-4.wav',
+      'samples/019-5.wav',
+      'samples/019-6.wav',
+      'samples/019-7.wav',
+      'samples/019-8.wav',
+      'samples/019-9.wav',
+      'samples/019-10.wav',
+      'samples/019-11.wav',
+      'samples/019-12.wav',
+      'samples/019-13.wav',
+      'samples/019-14.wav',
+      'samples/019-15.wav',
+      'samples/019-16.wav',
+      'samples/019-17.wav',
+];
+
+//encoding: string no, fingered pitch name and octave number, sounding octave, harmonic or not
+let newchords = [
+  //String 6
+  "chords/string-6_e1_octave-1.mp3",
+  "chords/string-6_e2_octave-2.mp3",
+  "chords/string-6_e2_octave-2_harmonic.mp3",
+  "chords/string-6_a1_octave-3_harmonic.mp3",
+  // String 5
+  "chords/string-5_e2_octave-2.mp3",
+  "chords/string-5_e3_octave-3.mp3",
+  "chords/string-5_e2_octave-3_harmonic.mp3",
+  "chords/string-5_e3_octave-3_harmonic.mp3",
+  // String 4
+  "chords/string-4_e2_octave-2.mp3",
+  "chords/string-4_e3_octave-3.mp3",
+  //String 3
+  "chords/string-3_e3_octave-3.mp3",
+  //String 2
+  "chords/string-2_e3_octave-3.mp3",
+  "chords/string-2_e4_octave-4.mp3",
+  //String 1
+  "chords/string-1_e3_octave-3.mp3",
+  "chords/string-1_e4_octave-4.mp3",
+  "chords/string-1_e4_octave-4_harmonic.mp3",
+  "chords/string-1_e3_octave-5_harmonic.mp3"
+];
+
+//THIS CALLES THE BUFFER CLASS
+let buffer = new Buffer(context, sines);
+let allSounds = buffer.getBuffer();
+
+var bigArray = [[0,1,2],[3],[1,4],[0,1,2,3,4]];
+var playback;
+
+function stopSound() {
+  playback = null;
+  soundy.stop();
+}
+
+function playSound(startingIndex) {
+  //this is the function called from the button.
+  playback = true;
+  var timeCounter = 0;
+  var nextTime = timing['timing'][timeCounter][0];
+  var totalTimings = timing['timing'].length;
+
+  for (var i = startingIndex; i < totalTimings; ++i) {
+    arrayPos = timeCounter % bigArray.length;
+    nextTime = timing['timing'][i][0];
+
+    bigArray[arrayPos].forEach(
+      function(x) {
+        soundy = new Sounder(context, buffer.getSound(x));
+        soundy.play(nextTime);
+    });
+    timeCounter += 1;
+    if (playback == null) {
+      i = totalTimings;
+    }
+  }
+}  
+
+
+
+
+
